0ea23562cb631acc1681d54d942545d0b9073dff
Jason Speet <jason.speet@gmail.com>
2019-12-02 23:05:46 -0500

    started tinkering my own stuff

---
+++ BarCrawl/BarCrawl/Models/Bar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarCrawl.Models
+{
+    public class Bar
+    {
+        public string Name { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal Latitude { get; set; }
+    }
+}
