560bd868fafbc58f46affee0856725943680f152
Jason Speet <jason.speet@gmail.com>
2019-12-04 15:03:10 -0500

    worked on mine

--- BarCrawlMine/BarCrawlMine/Controllers/HomeController.cs
+++ BarCrawlMine/BarCrawlMine/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BarCrawlMine.Models;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace BarCrawlMine.Controllers
 {
@@ -15,14 +18,40 @@ namespace BarCrawlMine.Controllers
             return View();
         }
 
+        public List<Bar> GetBars(string location)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp($"https://api.yelp.com/v3/businesses/search?term=bars&location={location}");
+            request.Headers.Add("Authorization", "Bearer 5AZ1TMhzZzb52DbbAMkydLPjNRSURY3x-DtC2o7qDjNTa2n96PSxuLZMmQoBy3WtX5q4EWUh4KQWVG1GG_nq_x2YLEssXjh5WF5kYw8E_VPmyRVMRfDHLwOYM0bXXXYx");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string ApiText = rd.ReadToEnd();
+            JToken tokens = JToken.Parse(ApiText);
+
+            List<JToken> ts = tokens["businesses"].ToList();
+            List<Bar> barList = new List<Bar>();
+
+            foreach(JToken t in ts)
+            {
+                Bar b = new Bar(t);
+                barList.Add(b);
+            }
+
+            return barList;
+        }
+
+
+
         public IActionResult Search()
         {
             return View();
         }
 
-        public IActionResult Result()
-        {
 
+
+        public IActionResult Result(string location)
+        {
+            List<Bar> bars = GetBars(location);
+            return View(bars);
         }
 
         public IActionResult Privacy()
---
+++ BarCrawlMine/BarCrawlMine/Models/Bar.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarCrawlMine.Models
+{
+    public class Bar
+    {
+        public string Name { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public string Location { get; set; }
+
+        public Bar()
+        {
+
+        }
+
+        public Bar(JToken t)
+        {
+            this.Name = t["name"].ToString();
+            this.Latitude = double.Parse(t["coordinates"]["latitude"].ToString());
+            this.Longitude = double.Parse(t["coordinates"]["longitude"].ToString());
+            this.Location = t["location"]["display_address"].ToString();
+        }
+    }
+
+    
+}
